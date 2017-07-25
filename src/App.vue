04d67bcb41dae017f7b39f9f15bcec20c4493bc3
power17 <1410837981@qq.com>
2017-07-25 18:36:27 +0800

    initialize

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <v-header></v-header>
+    </div>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
